ff54dc0499cf4a6c280d0203d721031501f09191
smart-k <k.g.gritsenko@gmail.com>
2016-03-02 12:19:19 +0200

    Renderer::_widget has been added.

--- framework/Renderer/Renderer.php
+++ framework/Renderer/Renderer.php
@@ -10,6 +10,9 @@ namespace Framework\Renderer;
 
 
 use Framework\ObjectPool;
+use Framework\ReflectionMethodNamedArgs;
+use Framework\Response\Response;
+use Framework\Exception\BadResponseTypeException;
 
 /**
  * Class Renderer
@@ -69,6 +72,7 @@ class Renderer extends ObjectPool
      */
     public function render($template_path, $data = array(), $wrap = true)
     {
+        $data['include'] = '$this->_widget';
         extract($data);
         // @TODO: provide all required vars or closures...
         if (file_exists($template_path)) {
@@ -87,5 +91,34 @@ class Renderer extends ObjectPool
             }
         }
         return $content;
+    }
+
+    /**
+     * Widget for view template
+     *
+     * @param string $controller_name
+     * @param string $action
+     * @param array $data
+     * @throws \Exception If obtained controller is not subclass of base controller.
+     */
+    private function _widget($controller_name, $action, $data = array())
+    {
+        $controllerReflection = new \ReflectionClass($controller_name);
+        if (!$controllerReflection->isSubclassOf('Framework\Controller\Controller')) {
+            throw new \Exception("Unknown controller " . $controllerReflection->name);
+        }
+        $action .= 'Action';
+        if ($controllerReflection->hasMethod($action)) {
+            // ReflectionMethod::invokeArgs() has overloaded in class ReflectionMethodNamedArgs
+            // Now it provides invoking with named arguments
+            $actionReflection = new ReflectionMethodNamedArgs($controller_name, $action);
+            $controller = $controllerReflection->newInstance();
+            $response = $actionReflection->invokeArgs($controller, $data);
+            if ($response instanceof Response) {
+                echo htmlspecialchars_decode($response['content']);
+            } else {
+                throw new BadResponseTypeException('Response type not known');
+            }
+        }
     }
 }
